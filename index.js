ca495b2e405fa82465bc14fde6423f36a1792e63
Disego <disegorachelm@gmail.com>
2024-04-05 11:53:21 +0200

    uodate displayMenuItems function

--- index.js
+++ index.js
@@ -4,34 +4,40 @@ const menu = {
     MainCourses: ["Margherita Pizza", "Spaghetti Carbonara"],
     Desserts: ["Tiramisu", "Cheesecake"]
 };
-
+const yourOrder = []; //initilaizing empty array to populate with order items
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
-
+    const menuContainer = document.getElementById("menu")
     // Loop through each category and its items in the menu object
-
+    Object.keys(menu).forEach(function(key){
         // Create an element to represent the category
-
+        const category = document.createElement("h2");
         // Set the text content of the category element to the category name
-
+        category.textContent = key;
         // Append the category element to the menu container
-
+        menuContainer.appendChild(category);
         // Create an element to represent a list of items
-
+        const menuItems = document.createElement("ul");
         // Append a list of items element to the menu container
-
+        menuContainer.appendChild(menuItems);
         // Loop through the items in the category and create list items
-
+        menu[key].forEach(foodItem => {
             // Create a list item element
-
+            const menuItem = document.createElement("li");
             // Set the text content of the list item element to the item name
-
+            menuItem.textContent = foodItem;
             // Attach a click event listener to the list item to add it to the order
-
+            menuItem.addEventListener('click', () => {
+                yourOrder.push(foodItem);
+                addToOrder(yourOrder)
+            })
             // Append the list item to the list of items
-
-            
+            menuItems.appendChild(menuItem);
+            })
+        }) 
+        
+        addToOrder(yourOrder);
 }
 
 // Callback function for adding an item to the order
